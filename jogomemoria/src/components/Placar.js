467c3483bb2664091e88f5082d452126cf95ab26
unknown <carlosruanaraujo789@gmail.com>
2023-01-20 01:47:12 -0300

    deixando menos feio kek - finalizado por enquanto

--- jogomemoria/src/components/Placar.js
+++ jogomemoria/src/components/Placar.js
@@ -1,23 +1,20 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 function Placar() {
-    const [teste,setteste] = useState(0)
- useEffect(() =>{
-    setteste(window.localStorage.getItem('placar'))
- },[teste])
+  const [resultado, setresultado] = useState(0);
+  useEffect(() => {
+    setresultado(window.localStorage.getItem("placar"));
+  }, [resultado]);
   return (
-    <div>
-        <div>PLACAR:</div>
-        {teste}
-        {
-            window.addEventListener('storage', () => {
-                console.log("wtf");
-                setteste(window.localStorage.getItem('placar'))
-             
-            })
-        }
+    <div className="placar">
+      <div>PLACAR:</div>
+      {<div className="resultado">{resultado}</div>}
+      {window.addEventListener("storage", () => {
+        console.log("wtf");
+        setresultado(window.localStorage.getItem("placar"));
+      })}
     </div>
-  )
+  );
 }
 
-export default Placar
+export default Placar;
--- jogomemoria/src/components/Tabuleiro.js
+++ jogomemoria/src/components/Tabuleiro.js
@@ -1,29 +1,22 @@
-import React, { useEffect, useState } from 'react'
-import Quadrado from './Quadrado'
+import React, { useEffect, useState } from "react";
+import Quadrado from "./Quadrado";
 
-
-function Tabuleiro({imagensTabuleiro}) {
-   
-  
+function Tabuleiro({ imagensTabuleiro }) {
   return (
-    
-    <div className='row d-flex justify-content-center    border border-danger tabuleiro'>
-        {
-            imagensTabuleiro.map((img,index) =>{
-               return (
-                <div className='col-2'>
-                <Quadrado img={img} index={index} quantidadeCartas={imagensTabuleiro.length}/>
-                </div>
-             
-               )
-            })
-        }
-         
-           
-        
-       
+    <div className="row d-flex justify-content-center  tabuleiro">
+      {imagensTabuleiro.map((img, index) => {
+        return (
+          <div className="col-2">
+            <Quadrado
+              img={img}
+              index={index}
+              quantidadeCartas={imagensTabuleiro.length}
+            />
+          </div>
+        );
+      })}
     </div>
-  )
+  );
 }
 
-export default Tabuleiro
+export default Tabuleiro;
